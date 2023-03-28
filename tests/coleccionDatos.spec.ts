dd91a7fa4339a10bbbbb04753a155eb0c9a2a399
Pablo Urquia <alu0101398327@ull.edu.es>
2023-03-28 08:14:15 +0000

    Primer push

--- tests/coleccionDatos.spec.ts
+++ tests/coleccionDatos.spec.ts
@@ -6,17 +6,17 @@ import {ColeccionDatos} from '../src/coleccionDatos.js';
 describe('ColeccionDatos', () => {
   it('Se puede crear una ColeccionDatos', () => {
     let coleccion = new ColeccionDatos([]);
-    expect(coleccion.getDatos()).to.equal([]);
+    expect(coleccion.getDatos()).to.eql([]);
   });
   it('Se puede modificar una ColeccionDatos', () => {
     let coleccion = new ColeccionDatos([]);
     coleccion.addDatos(new ColeccionFunkos('Usuario 1',[]));
-    expect(coleccion.getDatos()).to.equal([new ColeccionFunkos('Usuario 1',[])]);
+    expect(coleccion.getDatos()).to.eql([new ColeccionFunkos('Usuario 1',[])]);
   });
   it('Se puede eliminar una ColeccionDatos', () => {
     let coleccion = new ColeccionDatos([]);
     coleccion.addDatos(new ColeccionFunkos('Usuario 1',[]));
     coleccion.removeDatos('Usuario 1');
-    expect(coleccion.getDatos()).to.equal([]);
+    expect(coleccion.getDatos()).to.eql([]);
   });
 });
